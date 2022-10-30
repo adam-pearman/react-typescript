b93dcc934d2e5f4571dac97a51e3f1507c207742
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-10-30 16:26:45 +0000

    use Todos component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import './App.css'
+import Todos from './components/Todos'
 
 function App() {
   return (
     <div className="App">
-      
+      <Todos items={['Learn React', 'Learn Typescript']} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
